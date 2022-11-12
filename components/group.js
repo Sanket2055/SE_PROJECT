7752fbdf7eac8cebebd8da1c1b14344ac8ab1321
Sanket2055 <sanketdiwate95@gmail.com>
2022-11-12 16:46:05 +0530

    functionalities of splitting , clearing and adding payment

--- components/group.js
+++ components/group.js
@@ -203,3 +203,115 @@ exports.deleteGroup = async (req, res) => {
         });
     }
 };
+// 
+exports.makeSettlement = async (req, res) => {
+    try {
+        var reqBody = new model.Settlement(req.body);
+        validator.notNull(reqBody.groupId);
+        validator.notNull(reqBody.settleTo);
+        validator.notNull(reqBody.settleFrom);
+        validator.notNull(reqBody.settleAmount);
+        validator.notNull(reqBody.settleDate);
+        const group = await model.Group.findOne({
+            _id: req.body.groupId,
+        });
+        if (!group) {
+            var err = new Error('Invalid Group Id');
+            err.status = 400;
+            throw err;
+        }
+
+        group.split[0][req.body.settleFrom] += req.body.settleAmount;
+        group.split[0][req.body.settleTo] -= req.body.settleAmount;
+
+        var id = await model.Settlement.create(reqBody);
+        var update_response = await model.Group.updateOne(
+            { _id: group._id },
+            { $set: { split: group.split } }
+        );
+
+        res.status(200).json({
+            message: 'Settlement successfully!',
+            status: 'Success',
+            update: update_response,
+            response: id,
+        });
+    } catch (err) {
+        logger.error(
+            `URL : ${req.originalUrl} | staus : ${err.status} | message: ${err.message}`
+        );
+        res.status(err.status || 500).json({
+            message: err.message,
+        });
+    }
+};
+
+exports.addSplit = async (
+    groupId,
+    expenseAmount,
+    expenseOwner,
+    expenseMembers
+) => {
+    var group = await model.Group.findOne({
+        _id: groupId,
+    });
+    group.groupTotal += expenseAmount;
+    group.split[0][expenseOwner] += expenseAmount;
+    expensePerPerson = expenseAmount / expenseMembers.length;
+    expensePerPerson =
+        Math.round((expensePerPerson + Number.EPSILON) * 100) / 100;
+
+    for (var user of expenseMembers) {
+        group.split[0][user] -= expensePerPerson;
+    }
+
+    let bal = 0;
+    for (val of Object.entries(group.split[0])) {
+        bal += val[1];
+    }
+    group.split[0][expenseOwner] -= bal;
+    group.split[0][expenseOwner] =
+        Math.round((group.split[0][expenseOwner] + Number.EPSILON) * 100) / 100;
+
+    return await model.Group.updateOne(
+        {
+            _id: groupId,
+        },
+        group
+    );
+};
+
+exports.clearSplit = async (
+    groupId,
+    expenseAmount,
+    expenseOwner,
+    expenseMembers
+) => {
+    var group = await model.Group.findOne({
+        _id: groupId,
+    });
+    group.groupTotal -= expenseAmount;
+    group.split[0][expenseOwner] -= expenseAmount;
+    expensePerPerson = expenseAmount / expenseMembers.length;
+    expensePerPerson =
+        Math.round((expensePerPerson + Number.EPSILON) * 100) / 100;
+
+    for (var user of expenseMembers) {
+        group.split[0][user] += expensePerPerson;
+    }
+
+    let bal = 0;
+    for (val of Object.entries(group.split[0])) {
+        bal += val[1];
+    }
+    group.split[0][expenseOwner] -= bal;
+    group.split[0][expenseOwner] =
+        Math.round((group.split[0][expenseOwner] + Number.EPSILON) * 100) / 100;
+
+    return await model.Group.updateOne(
+        {
+            _id: groupId,
+        },
+        group
+    );
+};

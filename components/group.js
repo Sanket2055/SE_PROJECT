8009acc6b60d64b291376aab18b1b40f34dd0f95
Sanket2055 <sanketdiwate95@gmail.com>
2022-11-12 16:42:56 +0530

    created edit group and delete group functionalities

--- components/group.js
+++ components/group.js
@@ -104,3 +104,102 @@ exports.findUserGroup = async (req, res) => {
         });
     }
 };
+exports.editGroup = async (req, res) => {
+    try {
+        var group = await model.Group.findOne({
+            _id: req.body.id,
+        });
+        if (!group || req.body.id == null) {
+            var err = new Error('Invalid Group Id');
+            err.status = 400;
+            throw err;
+        }
+
+        var editGroup = new model.Group(req.body);
+
+        editGroup.split = group.split;
+
+        if (
+            validator.notNull(editGroup.groupName) &&
+            validator.currencyValidation(editGroup.groupCurrency)
+        ) {
+            for (var user of editGroup.groupMembers) {
+                var memberCheck = await validator.userValidation(user);
+                if (!memberCheck) {
+                    var err = new Error('Invalid member id');
+                    err.status = 400;
+                    throw err;
+                }
+
+                if (!editGroup.split[0].hasOwnProperty(user)) {
+                    editGroup.split[0][user] = 0;
+                }
+            }
+
+            var ownerCheck = await validator.userValidation(
+                editGroup.groupOwner
+            );
+            if (!ownerCheck) {
+                var err = new Error('Invalid owner id');
+                err.status = 400;
+                throw err;
+            }
+
+            var update_response = await model.Group.updateOne(
+                {
+                    _id: req.body.id,
+                },
+                {
+                    $set: {
+                        groupName: editGroup.groupName,
+                        groupDescription: editGroup.groupDescription,
+                        groupCurrency: editGroup.groupCurrency,
+                        groupMembers: editGroup.groupMembers,
+                        groupCategory: editGroup.groupCategory,
+                        split: editGroup.split,
+                    },
+                }
+            );
+            res.status(200).json({
+                status: 'Success',
+                message: 'Group updated successfully!',
+                response: update_response,
+            });
+        }
+    } catch (err) {
+        logger.error(
+            `URL : ${req.originalUrl} | staus : ${err.status} | message: ${err.message}`
+        );
+        res.status(err.status || 500).json({
+            message: err.message,
+        });
+    }
+};
+
+exports.deleteGroup = async (req, res) => {
+    try {
+        const group = await model.Group.findOne({
+            _id: req.body.id,
+        });
+        if (!group) {
+            var err = new Error('Invalid Group Id');
+            err.status = 400;
+            throw err;
+        }
+        var delete_group = await model.Group.deleteOne({
+            _id: req.body.id,
+        });
+        res.status(200).json({
+            message: 'Group deleted successfully!',
+            status: 'Success',
+            response: delete_group,
+        });
+    } catch (err) {
+        logger.error(
+            `URL : ${req.originalUrl} | staus : ${err.status} | message: ${err.message}`
+        );
+        res.status(err.status || 500).json({
+            message: err.message,
+        });
+    }
+};
